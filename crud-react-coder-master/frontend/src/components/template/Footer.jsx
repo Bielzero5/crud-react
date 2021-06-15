10ee20a5ff06be859cdc493efcb82d342decd2f6
Gabriel Ferreira <gabrielnick59@gmail.com>
2021-06-15 20:25:10 -0300

    crud finalizado

---
+++ crud-react-coder-master/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Gabriel Ferreira e Rafael Goulart </strong>
+        </span>
+    </footer>
